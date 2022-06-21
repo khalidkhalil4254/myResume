efb8aa3703bcf2e3b2b9a4f861567e4a6f990602
khalid khalil <kalidkalil4254@gmail.com>
2022-06-21 08:58:52 +0200

    f

---
+++ js/index.js
@@ -0,0 +1,4 @@
+import * as name from "./methods.js";
+
+
+name.menu_btn.addEventListener('click',name.showMenu);
---
+++ js/methods.js
@@ -0,0 +1,32 @@
+/*these are the variables*/
+var menu_btn=document.getElementsByClassName('material-symbols-outlined')[0];
+var menu_container=document.getElementById('header_items');
+
+var counter=1;
+
+var showMenu=function(){
+    if(counter%2==0){
+        menu_container.style.animationName='disapeare';
+        menu_container.style.animationDuration='0.4s'
+        menu_container.style.animationDirection='reverse';
+         
+
+        setTimeout(function rr(){
+            menu_container.style.display='none';
+        },350);
+
+    }
+    else{
+        menu_container.style.animationName='apeare';
+        menu_container.style.animationDuration='0.4s'
+        menu_container.style.animationDirection='reverse';
+        menu_container.style.display='flex';
+    
+    }
+    counter++;
+}
+
+
+
+
+export{showMenu,menu_btn,menu_container}
