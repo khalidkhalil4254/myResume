38ab8a9836f59ad40385880486f2a2e770f59c41
khalid khalil <kalidkalil4254@gmail.com>
2022-06-23 06:49:05 +0200

    g

--- js/index.js
+++ js/index.js
@@ -1,29 +1,4 @@
 import * as name from "./methods.js";
+window.onscroll = function() {name.scrollFunction()};
 name.menu_btn.addEventListener('click',name.showMenu);
-
-
-
-
-
-/*scrolling back to top btn feature*/
-//Get the button
-var mybutton = document.getElementById("myBtn");
-
-function scrollFunction() {
-  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
-    mybutton.style.display = "block";
-  } else {
-    mybutton.style.display = "none";
-  }
-}
-
-// When the user scrolls down 20px from the top of the document, show the button
-window.onscroll = function() {scrollFunction()};
-
-// When the user clicks on the button, scroll to the top of the document
-function topFunction() {
-  document.body.scrollTop = 0;
-  document.documentElement.scrollTop = 0;
-}
-
-mybutton.addEventListener('click',topFunction);
+name.mybutton.addEventListener('click',name.topFunction);
--- js/methods.js
+++ js/methods.js
@@ -1,7 +1,7 @@
 /*these are the variables*/
 var menu_btn=document.getElementsByClassName('material-symbols-outlined')[0];
 var menu_container=document.getElementById('header_items');
-
+var mybutton = document.getElementById("myBtn");
 var counter=1;
 
 var showMenu=function(){
@@ -25,7 +25,18 @@ var showMenu=function(){
     }
     counter++;
 }
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+    document.body.scrollTop = 0;
+    document.documentElement.scrollTop = 0;
+  }
+  function scrollFunction() {
+    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+      mybutton.style.display = "block";
+    } else {
+      mybutton.style.display = "none";
+    }
+  }
 
 
-
-export{showMenu,menu_btn,menu_container}
+export{showMenu,menu_btn,menu_container,topFunction,scrollFunction,mybutton}
